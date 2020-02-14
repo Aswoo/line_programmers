24909c0731150d614630ca99900277c2f7f1085b
윤승우 <yhnu191919@gmail.com>
2020-02-14 16:08:24 +0900

    #02_feature_create_fragment_02

--- app/src/main/java/com/test/memoapp/ui/memos/MemosFragment.kt
+++ app/src/main/java/com/test/memoapp/ui/memos/MemosFragment.kt
@@ -1,7 +1,42 @@
 package com.test.memoapp.ui.memos
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.transition.TransitionInflater
+import com.test.memoapp.R
+import com.test.memoapp.databinding.MemosFragBinding
+import kotlinx.android.synthetic.main.memos_frag.*
 
 class MemosFragment : Fragment(){
 
+
+    lateinit var binding : MemosFragBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val dataBinding = DataBindingUtil.inflate<MemosFragBinding>(
+            inflater,
+            R.layout.memos_frag,
+            container,
+            false
+        )
+        binding = dataBinding
+        //sharedElementReturnTransition = TransitionInflater.from(context).inflateTransition(R.transition.move)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        fab_add_memo.setOnClickListener {
+            //findNavController().navigate
+        }
+    }
 }
--- app/src/main/java/com/test/memoapp/ui/memos/MemosViewModel.kt
+++ app/src/main/java/com/test/memoapp/ui/memos/MemosViewModel.kt
@@ -1,2 +1,100 @@
 package com.test.memoapp.ui.memos
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import com.test.Event
+import com.test.memoapp.data.Memo
+
+class MemosViewModel : ViewModel(){
+    private val _items = MutableLiveData<List<Memo>>().apply { value = emptyList() }
+    val items: LiveData<List<Memo>> = _items
+
+    private val _dataLoading = MutableLiveData<Boolean>()
+    val dataLoading: LiveData<Boolean> = _dataLoading
+
+    private val _currentFilteringLabel = MutableLiveData<Int>()
+    val currentFilteringLabel: LiveData<Int> = _currentFilteringLabel
+
+    private val _noTasksLabel = MutableLiveData<Int>()
+    val noTasksLabel: LiveData<Int> = _noTasksLabel
+
+    private val _noTaskIconRes = MutableLiveData<Int>()
+    val noTaskIconRes: LiveData<Int> = _noTaskIconRes
+
+    private val _tasksAddViewVisible = MutableLiveData<Boolean>()
+    val tasksAddViewVisible: LiveData<Boolean> = _tasksAddViewVisible
+
+    private val _snackbarText = MutableLiveData<Event<Int>>()
+    val snackbarMessage: LiveData<Event<Int>> = _snackbarText
+
+    // Not used at the moment
+    private val isDataLoadingError = MutableLiveData<Boolean>()
+
+    private val _openTaskEvent = MutableLiveData<Event<String>>()
+    val openTaskEvent: LiveData<Event<String>> = _openTaskEvent
+
+    private val _newTaskEvent = MutableLiveData<Event<Unit>>()
+    val newTaskEvent: LiveData<Event<Unit>> = _newTaskEvent
+
+    // This LiveData depends on another so we can use a transformation.
+    val empty: LiveData<Boolean> = Transformations.map(_items) {
+        it.isEmpty()
+    }
+
+    init {
+        // Set initial state
+        //loadTasks(true)
+    }
+
+    /**
+     * Sets the current task filtering type.
+     *
+     * @param requestType Can be [TasksFilterType.ALL_TASKS],
+     * [TasksFilterType.COMPLETED_TASKS], or
+     * [TasksFilterType.ACTIVE_TASKS]
+     */
+
+    private fun setFilter(
+        @StringRes filteringLabelString: Int, @StringRes noTasksLabelString: Int,
+        @DrawableRes noTaskIconDrawable: Int, tasksAddVisible: Boolean
+    ) {
+        _currentFilteringLabel.value = filteringLabelString
+        _noTasksLabel.value = noTasksLabelString
+        _noTaskIconRes.value = noTaskIconDrawable
+        _tasksAddViewVisible.value = tasksAddVisible
+    }
+
+    /**
+     * Called by the Data Binding library and the FAB's click listener.
+     */
+    fun addNewTask() {
+        _newTaskEvent.value = Event(Unit)
+    }
+
+    /**
+     * Called by Data Binding.
+     */
+    fun openTask(taskId: String) {
+        _openTaskEvent.value = Event(taskId)
+    }
+
+    fun showEditResultMessage(result: Int) {
+        when (result) {
+            /*
+            EDIT_RESULT_OK -> showSnackbarMessage(R.string.successfully_saved_task_message)
+            ADD_EDIT_RESULT_OK -> showSnackbarMessage(R.string.successfully_added_task_message)
+            DELETE_RESULT_OK -> showSnackbarMessage(R.string.successfully_deleted_task_message)
+
+             */
+        }
+    }
+
+    private fun showSnackbarMessage(message: Int) {
+        _snackbarText.value = Event(message)
+    }
+
+}
